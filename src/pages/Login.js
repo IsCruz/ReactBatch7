dace8aa5c185af4d250ca3d38deb4ec22dd93a3b
Israel Cruz Ibarra <israel.ibarra@ibm.com>
2019-11-12 10:09:49 -0600

    transform to hooks

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import Navbar from '../components/genericComponents/Navbar';
 import Jumbotron from '../components/genericComponents/Jumbotron';
 import Footer from '../components/genericComponents/Footer';
 import { getToken } from '../services/userServices';
 import Auth from '../helpers/Auth';
+
+function Example() {
+    // Declara una nueva variable de estado, la cual llamaremos “count”
+    const [count, setCount] = useState(0);
+  
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+const LoginWithHooks = (props) => {
+    // in hooks means [value of the state, call for update the state]; 
+    const [ValEmail, email] = useState('');
+    /* 
+        ValEmail = this.state.email;
+        email ? this.setState({email: value});
+    */
+    const [ValPassword, password] = useState('');
+    const [ValError, error] = useState(false);
+    const [ValMessage, errorMessage] = useState('');
+    // const [state, setstate] = useState(initialState);
+    function onChange(event) {
+        event.preventDefault();
+        const { name, value } = event.target;
+        if (name === 'password') {
+            password(value);
+        } else {
+            email(value);
+        }
+    }
+    const authUser = (event) => {
+        event.preventDefault();
+         // CALL API SERVICES
+         getToken({email: ValEmail, password: ValPassword})
+         .then(responseData => {
+             if(responseData.success === false) {
+                 error(true);
+                 errorMessage(responseData.message);
+             } else {
+                 error(false);
+                 errorMessage('');
+                 // Setup Auth info
+                 Auth.login(responseData, () => {
+                    props.history.push('/task');
+                 });
+             }
+         })
+    };
+    const ErrorHandler = () => {
+        if (ValError) {
+            return <h2>{ValMessage}</h2> ;
+        }
+        return '';
+    };
+        return (
+            <div>
+            <Navbar />
+            <Jumbotron title="Login" subtitle="Sign In" />
+            <div className="container">
+                <h2>
+                Please Login User
+                </h2>
+                <input type="text" name="email" value={ValEmail}
+                 placeholder="email" onChange={onChange}
+                />
+                <input type="password" name="password" value={ValPassword} 
+                placeholder="password" onChange={onChange}
+                />
+                <button onClick={authUser}>Login (please)</button>
+                
+                <div>
+                    <ErrorHandler/>
+                </div>
+            </div>
+            <Footer /> 
+        </div>
+        );
+};
 
 class Login extends React.Component {
     constructor(props) {
@@ -71,4 +153,5 @@ class Login extends React.Component {
         }
 
 }
-export default withRouter(Login);
+
+export default withRouter(LoginWithHooks);
